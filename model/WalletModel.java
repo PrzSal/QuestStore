61ef2211fb4236f55747161501ebdf25dde64291
Przemo8922 <przemyslawsalak@gmail.com>
2017-08-30 17:17:41 +0200

    Add setIsUsed method

--- model/WalletModel.java
+++ model/WalletModel.java
@@ -1,29 +1,44 @@
-// package model;
-import java.util.LinkedList;
+package model;
 
-public class WalletModel{
+import java.util.LinkedList;
+import java.lang.NullPointerException;
+
+public class WalletModel {
+
     private Integer coolCoins;
     private LinkedList<ArtifactModel> artifactsList;
 
-    public WalletModel(Integer coolCoins){
+    public WalletModel(Integer coolCoins) {
 
         this.coolCoins = 0;
         this.artifactsList = new LinkedList<ArtifactModel>();
     }
 
-    public void removeCoolCoins(Integer amount){
+    public void removeCoolCoins(Integer amount) {
 
         this.coolCoins -= amount;
     }
 
-    public void addCoolCoins(Integer amount){
+    public void addCoolCoins(Integer amount) {
 
         this.coolCoins += amount;
     }
 
-    public void addBoughtArtifact(ArtifactModel artifact){
+    public void addBoughtArtifact(ArtifactModel artifact) {
 
         this.artifactsList.add(artifact);
     }
 
+    public void setIsUsed(String name) throws NullPointerException {
+
+        for (ArtifactModel artifact : this.artifactsList) {
+
+            if (artifact.title.equals(name)) {
+                artifact.isUsed = true;
+            }
+        throw new NullPointerException("Artifact not found \n");
+
+        }
+
+    }
 }
