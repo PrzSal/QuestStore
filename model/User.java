b045f78e90683b34f52db08f4479db469e0f966e
PawelSyktus <pawel.syktus@gmail.com>
2017-08-30 16:18:51 +0200

    Add getName() method

--- model/User.java
+++ model/User.java
@@ -1,1 +1,16 @@
 package model;
+
+public abstract User {
+
+    protected final Integer USER_ID;
+    public static Integer counter = 0;
+    protected String name;
+    protected String surname;
+    protected String email;
+    protected String login;
+    protected String password;
+
+    public String getName(String name){
+        return name;
+    }
+}
