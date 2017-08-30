6ceae4582ef69d51170f0fc6ca00585461dd1ed9
jaguuuar <sirkamson@gmail.com>
2017-08-30 22:50:47 +0200

    Create Test module

---
+++ Test.java
@@ -0,0 +1,10 @@
+import controller.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        AdminController admin = new AdminController();
+
+        admin.createMentor();
+    }
+}
