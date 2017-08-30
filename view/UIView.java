54de06891a936c6b7ff22be814403445a0a39a23
jaguuuar <sirkamson@gmail.com>
2017-08-30 21:08:23 +0200

    Add continueButton method

--- view/UIView.java
+++ view/UIView.java
@@ -1,3 +1,6 @@
+// package view;
+
+import java.util.Scanner;
 
 public class UIView {
 
@@ -5,5 +8,30 @@ public class UIView {
         System.out.println(message);
     }
 
-    
+    public static String getInput(String message) {
+
+        try(Scanner input = new Scanner(System.in)) {
+            System.out.print(message);
+
+            return input.nextLine();
+        }
+    }
+
+    public static void continueButton() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Press enter to continue ...");
+        input.nextLine();
+
+    }
+
+    public static void main(String[] args) {
+
+        continueButton();
+        String input = getInput("Type name: ");
+
+
+
+
+    }
 }
