b58868a3d582e0f70e37b561a0acae789e762d78
szmytke <szmytke@gmail.com>
2017-08-30 17:36:27 +0200

    Add getQuestst().

--- model/StudentModel.java
+++ model/StudentModel.java
@@ -41,3 +41,6 @@ public class StudentModel extends User {
     public void addExperience(Integer experience){
         this.experience += experience;
     }
+    public LinkedList<QuestModel> getQuestst(){
+        return questsList;
+    }
