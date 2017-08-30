406f92b14082105b27acded0b16daa06d5eb9d00
jaguuuar <sirkamson@gmail.com>
2017-08-30 23:09:36 +0200

    fix constructor

--- model/AdminModel.java
+++ model/AdminModel.java
@@ -2,11 +2,13 @@ package model;
 
 public static AdminModel extends User {
 
+    public static Integer counter = 0;
+
     public AdminModel(String name, String surname, String email){
         this.name = name;
         this.surname = surname;
         this.email = email;
-        this.USER_ID = counter++;
+        this.USER_ID = "A_" + counter++;
         this.login = setDefaultLogin();
         this.pasword = setDeafultPassword();
 
