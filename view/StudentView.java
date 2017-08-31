2c6eb47eba1f8aced92349bdf59926f93ee73a55
jaguuuar <sirkamson@gmail.com>
2017-08-31 23:21:27 +0200

    Edit showMenu method

--- view/StudentView.java
+++ view/StudentView.java
@@ -10,4 +10,14 @@ public class StudentView {
     public static void showExperience(Integer experience) {
         System.out.println(experience);
     }
+
+    public void showMenu() {
+        String menu = "1) Create mentor.\n" +
+                      "2) Edit mentor.\n" +
+                      "3) Create class.\n" +
+                      "0) EXIT";
+
+        System.out.println(menu);
+    }
+
 }
