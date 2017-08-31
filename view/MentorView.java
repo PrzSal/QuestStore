ef53771096550ded4168af67c1d8a745989623bf
szmytke <szmytke@gmail.com>
2017-08-31 22:17:27 +0200

    Add MentorView class.

---
+++ view/MentorView.java
@@ -0,0 +1,14 @@
+package view;
+
+public class MentorView {
+
+    public void printMenu() {
+
+        System.out.println("Mentor Menu: \n" 
+                            + "1. Add new student.\n"
+                            + "2. Add new quest.\n"
+                            + "3. Add new artifact\n"
+                            + "0. Exit");
+    }
+
+}
