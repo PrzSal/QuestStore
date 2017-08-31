69ed82e671902fe63f4090b8919f46769f1e895d
PawelSyktus <pawel.syktus@gmail.com>
2017-08-31 14:58:28 +0200

    Add remove method

--- dao/AbstractDAO.java
+++ dao/AbstractDAO.java
@@ -1,4 +1,8 @@
+package dao;
+
 import interfaces.*;
+import model.*;
+
 import java.util.Iterator;
 
 public abstract class AbstractDAO<T> implements Iterator, DAO<T> {
@@ -9,20 +13,30 @@ public abstract class AbstractDAO<T> implements Iterator, DAO<T> {
         objectsList.add(object);
     }
 
-    public void removeObject(T object) {
-        objectsList.remove(object)
+    public void removeObject(String userID) {
+
+        Iterator<TodoItem> iter = objectsList.iterator();
+
+        while (iter.hasNext()) {
+          T object = iter.next();
+
+          if (object.getUserID().equals(userID) {
+            iter.remove();
+          }
+        }
     }
 
     public void setObjectList(LinkedList<T> newObjectsList) {
-        objectsList.
+        objectsList = newObjectsList;
     }
 
     public LinkedList<T> getObjectList() {
-
+        return objectsList;
     }
 
     public T getObjectBy(String userID) {
 
+        for (T object )
     }
 
     public Iterator getIterator() {
