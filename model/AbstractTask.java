7bc6793c52f9b8a8e6b15586f8040d024b82e792
Przemo8922 <przemyslawsalak@gmail.com>
2017-08-31 13:28:57 +0200

    Add method getTitle

---
+++ model/AbstractTask.java
@@ -0,0 +1,13 @@
+package model;
+
+public class AbstractTask<T> {
+    protected String title;
+    protected Integer price;
+    protected <T> category;
+
+    public String getTitle(){
+
+        return tittle;
+    }
+
+}
