43b2435cbf04fe4d59f9700926c089450b44d139
jaguuuar <sirkamson@gmail.com>
2017-08-31 11:04:57 +0200

    Add searchMentor method

--- controller/AdminController.java
+++ controller/AdminController.java
@@ -15,6 +15,29 @@ public class AdminController {
         return MentorModel
     }
 
+    // public ArrayList<MentorModel> searchMentorBySurname() {
+    //
+    // }
+    //
+    //
+    // public void editMentor() {
+    //
+    //     String surname = getInput("Enter mentor surname: ")
+    //
+    //     ArrayList<MentorModel> searchMentor = new ArrayList<MentorModel>();
+    //     for(MentorModel mentor: MentorList) {
+    //         if(mentor.getSurame().equals(surname))
+    //             searchMentor.add(mentor)
+    //
+    //     }
+    //
+    //
+    //
+    //
+    // }
+
+
+
     // public static void main(String[] args) {
     //     createMentor();
     //
