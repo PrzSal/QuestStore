7bf365ecbc8c2e926a8eb5e30460c27e37fe5455
jaguuuar <sirkamson@gmail.com>
2017-08-31 23:20:57 +0200

    Add StudentView and UIView fields

--- controller/StudentController.java
+++ controller/StudentController.java
@@ -7,6 +7,9 @@ import model.*;
 import dao.ArtifactDAO;
 import view.*;
 
+private static UIView view = new UIView();
+private static StudentView studentView = new StudentView();
+
 public class StudentController {
 
     public void buyArtifact(StudentModel studentModel, ArtifactDAO artifactDAO) {
@@ -28,7 +31,7 @@ public class StudentController {
             studentModel.getWallet().addBoughtArtifact(artifactToBuy);
             studentModel.getWallet().removeCoolCoins(artifactPrice);
 
-        } 
+        }
         else {
             uiView.printMessage("You don't have enought cool coins");
         }
@@ -43,5 +46,49 @@ public class StudentController {
 
     public void showWallet(StudentModel studentModel) {
         studentModel.getWalletModel().showWallet();
+    }
+
+    public void startMenu(String operation, ArtifactDAO artifactDao, QuestDAO questDao) {
+
+        switch(operation) {
+
+        case CREATE_MENTOR :
+            this.createMentor();
+            view.continueButton();
+            break;
+
+        case EDIT_MENTOR :
+            // ArrayList<MentorModel> mentorSurname = view.getInput("Find mentor by surname: ");
+            // this.getMentorBySurname(String mentorSurname)
+            view.continueButton();
+            break;
+
+        case CREATE_CLASS :
+            this.createClass();
+            view.continueButton();
+            break;
+
+        case EXIT:
+            break;
+
+         default :
+            view.printMessage("No option! Try Again!\n");
+            view.continueButton();
+        }
+
+    }
+
+    public void startStudentController(ArtifactDAO artifactDao, QuestDAO questDao) {
+
+        String operation;
+
+        do {
+            view.clearScreen();
+            adminView.showMenu();
+            operation = view.getInput("Choice option: ");
+            view.clearScreen();
+            this.startMenu(operation, artifactDao, questDao);
+        } while (!operation.equals(EXIT));
+
     }
 }
