d2774a98224317a4c30a6365f3d20abe6203fbbd
jaguuuar <sirkamson@gmail.com>
2017-08-31 10:37:34 +0200

    Add set and get classID method

--- model/MentorModel.java
+++ model/MentorModel.java
@@ -3,7 +3,7 @@ package model;
 public static MentorModel extends User {
 
     public static Integer counter = 0;
-    private String teamID;
+    private String classID;
 
     public MentorModel (String name, String surname, String email){
         this.name = name;
@@ -12,6 +12,13 @@ public static MentorModel extends User {
         this.USER_ID = "M_" + counter++;
         this.login = setDefaultLogin();
         this.pasword = setDeafultPassword();
-        this.teamID = null;
+        this.classID = null;
+    }
 
+    public String getClassID() {
+        return classID;
+    }
+
+    public void setClassID(String classID) {
+        this.classID = classID;
     }
