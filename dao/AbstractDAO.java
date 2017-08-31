77425a99a7159ce259ca37d3e50c846755088020
PawelSyktus <pawel.syktus@gmail.com>
2017-08-31 14:13:56 +0200

    Add AbsractDAO class

--- dao/AbstractDAO.java
+++ dao/AbstractDAO.java
@@ -1,12 +1,31 @@
 import interfaces.*;
+import java.util.Iterator;
 
-public abstract class AbstractDAO implements Iteratr, DAO {
+public abstract class AbstractDAO<T> implements Iterator, DAO<T> {
 
-    public void addObject(T);
-    public void removeObject(T);
-    public void setObjectList(LinkedList<T>);
-    public LinkedList<T> getObjectList();
-    public T getObjectBy(String userID);
-    public Iterator getIterator();
+    protected LinkedList<T> objectsList = new LinkedList<T>;
+
+    public void addObject(T object) {
+        objectsList.add(object);
+    }
+
+    public void removeObject(T object) {
+    }
+
+    public void setObjectList(LinkedList<T>) {
+
+    }
+
+    public LinkedList<T> getObjectList() {
+
+    }
+
+    public T getObjectBy(String userID) {
+
+    }
+
+    public Iterator getIterator() {
+        return new Iterator();
+    }
 
 }
--- interfaces/DAO.java
+++ interfaces/DAO.java
@@ -2,9 +2,9 @@ package interfaces;
 
 public interface DAO<T> {
 
-    public void addObject(T);
-    public void removeObject(T);
-    public void setObjectList(LinkedList<T>);
+    public void addObject(T object);
+    public void removeObject(T object);
+    public void setObjectList(LinkedList<T> objectsList);
     public LinkedList<T> getObjectList();
     public T getObjectBy(String userID);
 
