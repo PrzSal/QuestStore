17ad1ca45db5ffc9dd570e530226b167619cdd74
Przemo8922 <przemyslawsalak@gmail.com>
2017-08-31 13:45:27 +0200

    Add method getCategory

--- model/AbstractTask.java
+++ model/AbstractTask.java
@@ -3,7 +3,7 @@ package model;
 public class AbstractTask<T> {
     protected String title;
     protected Integer price;
-    protected <T> category;
+    protected T category;
 
     public String getTitle() {
 
@@ -23,5 +23,10 @@ public class AbstractTask<T> {
     public void setPrice(Integer newPrice) {
 
         price = newPrice;
+    }
+
+    public T getCategory() {
+
+        return category;
     }
 }
