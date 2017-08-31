649cc0e942c177acc79b6327d5868043a1f02eb8
Przemo8922 <przemyslawsalak@gmail.com>
2017-08-31 11:26:08 +0200

    Add method toString

--- model/ArtifactModel.java
+++ model/ArtifactModel.java
@@ -11,4 +11,23 @@ public class ArtifactModel extends AbstractTask<T> {
         this.artifactCategory = artifactCategory;
         this.isUsed = false;
     }
+
+    @Override
+    public String toString() {
+
+        String isUsedState = "";
+
+        if (this.isUsed.equals(true)) {
+            isUsedState = "[X]";
+        }
+
+        else {
+            isUsedState = "[ ]";
+        }
+
+
+        String artifactString = "Tittle artifact: " + this.tittle + " Price: " + this.price + "State use: " + isUsedState;
+
+        return artifactString;
+    }
 }
