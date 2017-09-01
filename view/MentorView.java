516c65743061530e158434d54989e2f84c5e4b4b
PawelSyktus <pawel.syktus@gmail.com>
2017-09-01 05:39:21 +0200

    Add classID in toString mehtod

--- view/MentorView.java
+++ view/MentorView.java
@@ -13,7 +13,7 @@ public class MentorView {
 
     public void showMentorList(String mentorDaoString) {
 
-        String headline = "\033[3;33mNo | User ID | Name | Surname | Email | Login | Password\033[0m\n";
+        String headline = "\033[3;33mNo | User ID | Name | Surname | Email | Class ID | Login | Password\033[0m\n";
         System.out.println(headline);
         System.out.println(mentorDaoString);
     }
