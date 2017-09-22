d45f003c43f9036a00f450eac9e50029ce841025
PawelSyktus <pawel.syktus@gmail.com>
2017-09-22 05:15:27 +0200

    Refactor code with Java style

--- src/main/java/com/codecool/dream_is_green/view/WalletView.java
+++ src/main/java/com/codecool/dream_is_green/view/WalletView.java
@@ -1,11 +1,8 @@
 package com.codecool.dream_is_green.view;
 
-import java.util.LinkedList;
-
 public class WalletView {
 
     public void showLineInWallet(String message) {
-
         System.out.println(message);
 
     }
