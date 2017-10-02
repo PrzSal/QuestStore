c5d1517641c5fb43c72adfaa4e43f1935ade3e30
PawelSyktus <pawel.syktus@gmail.com>
2017-10-02 13:36:38 +0200

    Update FactoryDAO.java

--- src/main/java/com/codecool/dream_is_green/dao/FactoryDAO.java
+++ src/main/java/com/codecool/dream_is_green/dao/FactoryDAO.java
@@ -10,35 +10,30 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class FactoryDAO {
-
-    UIView uiView = new UIView();
+    
+    private UIView uiView = new UIView();
 
     public void checkIsDatabase() {
-      
         File database = new File("quest_store.db");
         boolean exists = database.exists();
 
         if (!exists) {
-          
             createDatabase(readSqlFile());
         }
     }
 
     public String readSqlFile() {
-
         String querySql = "";
         File fileSql = new File("quest_store.db.sql");
 
         try {
             Scanner readLine = new Scanner(fileSql);
-
+           
             while (readLine.hasNext()) {
-
                 querySql += readLine.next() + "\n";
 
             }
         } catch (FileNotFoundException e) {
-
             uiView.printMessage("File not found.");
         }
 
@@ -46,7 +41,6 @@ public class FactoryDAO {
     }
 
     public void createDatabase(String querySql) {
-
         Connection connection = null;
         Statement statement = null;
 
