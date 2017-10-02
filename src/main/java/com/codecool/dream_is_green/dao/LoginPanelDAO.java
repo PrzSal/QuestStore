0e31965388e6f90b862c628486664fa37a6333f1
PawelSyktus <pawel.syktus@gmail.com>
2017-10-02 15:08:23 +0200

    Add getUserDetails method

--- src/main/java/com/codecool/dream_is_green/dao/LoginPanelDAO.java
+++ src/main/java/com/codecool/dream_is_green/dao/LoginPanelDAO.java
@@ -1,54 +1,43 @@
 package com.codecool.dream_is_green.dao;
 
-import com.codecool.dream_is_green.controller.AdminController;
-import com.codecool.dream_is_green.controller.MentorController;
-import com.codecool.dream_is_green.controller.StudentController;
-import com.codecool.dream_is_green.view.UIView;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-public class LoginDAO {
+public class LoginPanelDAO {
 
-    private static final String EXIT = "E";
+    public String[] getUserDetails(String login) {
 
-    public static void loginIntoSystem() {
+        Connection conn;
+        Statement stat;
 
-        UIView view = new UIView();
-        String operation;
+        try {
 
-        do {
+            conn = DatabaseConnection.getConnection();
+            stat = conn.createStatement();
 
-            view.clearScreen();
-            operation = view.getInput("~~~ Welcome in CODECOOL SHOP :)\n" +
-                    "~~~ Login into system: press ENTER to continue\n" +
-                    "~~~ Exit: press E\n");
-            operation = operation.toUpperCase();
+            String query = "SELECT user_type, password FROM UsersTable WHERE login = '" + login + "';" ;
+            ResultSet result = stat.executeQuery(query);
+            String user_type = "";
+            String password = "";
+            String[] userLoginDetails = {"", ""};
 
-            if (!operation.equals(EXIT)) {
-                String login = view.getInput("Enter login: ");
-                String password = view.getInput("Enter password: ");
-               chooseUserPanel(login, password);
+            while (result.next()) {
+                user_type = result.getString("user_type");
+                password = result.getString("user_type");
             }
 
-        } while (!operation.equals(EXIT));
-    }
+            userLoginDetails[0] = user_type;
+            userLoginDetails[1] = password;
 
+            result.close();
+            stat.close();
 
-    private static void chooseUserPanel(String login, String password) {
+            return  userLoginDetails;
 
-        if (mentor != null && mentor.getPassword().equals(password)) {
-            MentorController mentorController = new MentorController();
-            mentorController.startMentorController();
-
-        } else if (admin != null && admin.getPassword().equals(password)) {
-            AdminController adminController = new AdminController();
-            adminController.startAdminController();
-
-        } else if (student != null && student.getPassword().equals(password)) {
-            StudentController studentController = new StudentController();
-            studentController.startStudentController(student);
-
-        } else {
-            view.printMessage("\nWrong login or password!\n");
-            view.pressToContinue();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return null;
     }
 }
