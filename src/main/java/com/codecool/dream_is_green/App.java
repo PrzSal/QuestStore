c17cb806c16732a14d5aa8dfdd7fd5d4829c9f05
PawelSyktus <pawel.syktus@gmail.com>
2017-10-03 10:35:56 +0200

    Check if database exist

--- src/main/java/com/codecool/dream_is_green/App.java
+++ src/main/java/com/codecool/dream_is_green/App.java
@@ -1,10 +1,13 @@
 import com.codecool.dream_is_green.dao.DatabaseConnection;
 import com.codecool.dream_is_green.controller.LoginPanelController;
+import com.codecool.dream_is_green.dao.FactoryDAO;
 
 public class App {
 
     public static void main(String[] args) {
 
+        FactoryDAO factoryDao = new FactoryDAO();
+        factoryDao.checkIsDatabase();
         DatabaseConnection.getConnection();
         LoginPanelController loginPanel = new LoginPanelController();
         loginPanel.loginIntoSystem();  // admin/admin, mentor/mentor, student/student
--- src/main/java/com/codecool/dream_is_green/dao/AdminDAO.java
+++
@@ -1,6 +0,0 @@
-package com.codecool.dream_is_green.dao;
-
-import com.codecool.dream_is_green.model.AdminModel;
-
-public class AdminDAO extends AbstractDAO<AdminModel> {
-    }
