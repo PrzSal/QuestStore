e56f8a4c69ed2a329e18f65f1e28960cf2bb45f7
PawelSyktus <pawel.syktus@gmail.com>
2017-10-03 06:15:17 +0200

    Change return type to Object not Array with strings in getUserDetails method

--- src/main/java/com/codecool/dream_is_green/dao/LoginPanelDAO.java
+++ src/main/java/com/codecool/dream_is_green/dao/LoginPanelDAO.java
@@ -1,12 +1,13 @@
 package com.codecool.dream_is_green.dao;
 
+import com.codecool.dream_is_green.model.LoginDetailsModel;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class LoginPanelDAO {
 
-    public String[] getUserDetails(String login) {
+    public LoginDetailsModel getUserDetails(String login) {
 
         Connection conn;
         Statement stat;
@@ -18,22 +19,20 @@ public class LoginPanelDAO {
 
             String query = "SELECT user_type, password FROM UsersTable WHERE login = '" + login + "';" ;
             ResultSet result = stat.executeQuery(query);
-            String user_type = "";
+            String userType = "";
             String password = "";
-            String[] userLoginDetails = {"", ""};
 
             while (result.next()) {
-                user_type = result.getString("user_type");
+                userType = result.getString("user_type");
                 password = result.getString("user_type");
             }
 
-            userLoginDetails[0] = user_type;
-            userLoginDetails[1] = password;
+            LoginDetailsModel loginDetails = new LoginDetailsModel(userType, password);
 
             result.close();
             stat.close();
 
-            return  userLoginDetails;
+            return  loginDetails;
 
         } catch (Exception e) {
             e.printStackTrace();
