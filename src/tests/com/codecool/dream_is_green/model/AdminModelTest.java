e8006f9a688dcd2f7110d1bacd35df583d7e1930
Kamil Postrożny <kamilp009@gmail.com>
2017-10-04 13:26:58 +0200

    Create AdminModelTest

---
+++ src/tests/com/codecool/dream_is_green/model/AdminModelTest.java
@@ -0,0 +1,11 @@
+package com.codecool.dream_is_green.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminModelTest {
+
+    @Test
+    public void test
+}
