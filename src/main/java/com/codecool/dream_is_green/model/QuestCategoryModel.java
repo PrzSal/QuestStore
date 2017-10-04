d0bfe131e6dd2b8e70652b1b57b871369940bee0
PawelSyktus <pawel.syktus@gmail.com>
2017-10-04 14:42:59 +0200

    Remove getter to name attribute

--- src/main/java/com/codecool/dream_is_green/model/QuestCategoryModel.java
+++ src/main/java/com/codecool/dream_is_green/model/QuestCategoryModel.java
@@ -2,13 +2,7 @@ package com.codecool.dream_is_green.model;
 
 public class QuestCategoryModel extends AbstractTaskCategory {
 
-    private String name;
-
     public QuestCategoryModel(String name) {
         super(name);
-    }
-
-    public String getName() {
-        return name;
     }
 }
