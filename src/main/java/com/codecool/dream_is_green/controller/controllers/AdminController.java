bb48fabe0dc4cb4bdb177d6d315fbf045eeaaa58
Przemo8922 <przemyslawsalak@gmail.com>
2017-11-16 15:51:13 +0100

    refactor admin controller, connect show mentors and add mentors-final version

--- src/main/java/com/codecool/dream_is_green/controller/controllers/AdminController.java
+++ src/main/java/com/codecool/dream_is_green/controller/controllers/AdminController.java
@@ -2,6 +2,8 @@ package com.codecool.dream_is_green.controller.controllers;
 
 import com.codecool.dream_is_green.dao.ClassDAO;
 import com.codecool.dream_is_green.dao.MentorDAO;
+import com.codecool.dream_is_green.model.MentorModel;
+import com.codecool.dream_is_green.model.PreUserModel;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
@@ -21,11 +23,13 @@ public class AdminController implements HttpHandler {
 
         for (String action : actionData.keySet()) {
             if (action.equals("add_class")) {
-                add(httpExchange);
+                addClass(httpExchange);
             } else if (action.equals("edit")) {
                 edit(httpExchange, actionData.get(action));
             } else if (action.equals("show_classes")) {
                 showClasses(httpExchange);
+            } else if (action.equals("add_mentor")) {
+                addMentor(httpExchange);
             }  else if (action.equals("show_mentors")) {
                 showMentors(httpExchange);
             } else {
@@ -60,6 +64,38 @@ public class AdminController implements HttpHandler {
         os.close();
     }
 
+    private void addMentor(HttpExchange httpExchange) throws IOException {
+        String method = httpExchange.getRequestMethod();
+        String redirect = "";
+
+        if (method.equals("POST")) {
+            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            String formData = br.readLine();
+            MentorDAO mentorDAO = new MentorDAO();
+            String name = parseFormData(formData).get(0);
+            String surname = parseFormData(formData).get(1);
+            String email = parseFormData(formData).get(2);
+            String login = parseFormData(formData).get(3);
+            String password = parseFormData(formData).get(4);
+            String className = parseFormData(formData).get(5);
+            mentorDAO.insertMentor(new PreUserModel(name, surname, email, login, password, className));
+            redirect = "<meta http-equiv=\"refresh\" content=\"0; url=/admin/show_mentors/\" />";
+        }
+
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("static/templates/admin/admin_add_mentor.html.twig");
+        JtwigModel model = JtwigModel.newModel();
+        ClassDAO classDAO = new ClassDAO();
+        classDAO.loadClasses();
+        model.with("classModels", classDAO.getObjectList());
+        model.with("redirect", redirect);
+        String response = template.render(model);
+
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
 
     private void showMentors(HttpExchange httpExchange) throws IOException {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("static/templates/admin/admin_show_mentors.html.twig");
@@ -76,7 +112,7 @@ public class AdminController implements HttpHandler {
         os.close();
     }
 
-    private void add(HttpExchange httpExchange) throws IOException {
+    private void addClass(HttpExchange httpExchange) throws IOException {
         String method = httpExchange.getRequestMethod();
         String redirect = "";
 
@@ -85,7 +121,7 @@ public class AdminController implements HttpHandler {
             BufferedReader br = new BufferedReader(isr);
             String formData = br.readLine();
             ClassDAO classDao = new ClassDAO();
-            classDao.insertClass(parseFormData(formData));
+            classDao.insertClass(parseFormData(formData).get(0));
             redirect = "<meta http-equiv=\"refresh\" content=\"0; url=/admin/show_classes/\" />";
         }
 
@@ -123,18 +159,21 @@ public class AdminController implements HttpHandler {
         return actionData;
     }
 
-    private String parseFormData(String formData) {
+    private ArrayList<String> parseFormData(String formData) {
         System.out.println(formData);
-        String className;
+        ArrayList<String> dataToModel = new ArrayList<>();
 
         String[] pairs = formData.split("&");
-
+        int i = 0;
         try {
-            className = new URLDecoder().decode(pairs[0].split("=")[1], "UTF-8");
+            for (String pair : pairs) {
+                dataToModel.add(new URLDecoder().decode(pair.split("=")[1], "UTF-8"));
+
+            }
         } catch (ArrayIndexOutOfBoundsException | UnsupportedEncodingException e) {
             return null;
         }
 
-        return className;
+        return dataToModel;
     }
 }
