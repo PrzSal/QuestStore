fd2970a8652a46d8a6b98c2edbd5523061019292
Przemo8922 <przemyslawsalak@gmail.com>
2017-11-21 23:37:46 +0100

    refactor MailBoxModel

--- src/main/java/com/codecool/dream_is_green/model/MailBoxModel.java
+++ src/main/java/com/codecool/dream_is_green/model/MailBoxModel.java
@@ -4,12 +4,14 @@ public class MailBoxModel extends User {
 
     private String content;
     private String header;
+    private Integer idRecipient;
 
     public MailBoxModel(int userID, String name, String surname, String email,
-                        String login, String password, String header, String content) {
+                        String login, String password, String header, String content, Integer idRecipient) {
         super(userID, name, surname, email, login, password);
         this.content = content;
         this.header = header;
+        this.idRecipient = idRecipient;
     }
 
     public String getContent() {
@@ -18,5 +20,9 @@ public class MailBoxModel extends User {
 
     public String getHeader() {
         return header;
+    }
+
+    public Integer getIdRecipient() {
+        return idRecipient;
     }
 }
