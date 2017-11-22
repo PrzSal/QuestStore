568c7121e3939a2da09319afd8e8d36994884f25
Przemo8922 <przemyslawsalak@gmail.com>
2017-11-22 15:03:04 +0100

    add field and constructor to TeamShoppingModel

--- src/main/java/com/codecool/dream_is_green/model/TeamShoppingModel.java
+++ src/main/java/com/codecool/dream_is_green/model/TeamShoppingModel.java
@@ -1,4 +1,59 @@
 package com.codecool.dream_is_green.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class TeamShoppingModel {
+    List<StudentModel> studentModels = new LinkedList<>();
+    ArtifactModel artifactModel;
+    Integer teamId;
+    String nameTeam;
+    String vote;
+
+    public TeamShoppingModel() {
+    }
+
+    public TeamShoppingModel(List<StudentModel> studentModels, Integer teamId, String nameTeam) {
+        this.studentModels = studentModels;
+        this.teamId = teamId;
+        this.nameTeam = nameTeam;
+    }
+
+    public TeamShoppingModel(List<StudentModel> studentModels, Integer teamId, String nameTeam, String vote) {
+        this.studentModels = studentModels;
+        this.teamId = teamId;
+        this.nameTeam = nameTeam;
+        this.vote = vote;
+    }
+
+    public TeamShoppingModel(List<StudentModel> studentModels, ArtifactModel artifactModel, Integer teamId, String nameTeam) {
+        this.studentModels = studentModels;
+        this.artifactModel = artifactModel;
+        this.teamId = teamId;
+        this.nameTeam = nameTeam;
+    }
+
+    public List<StudentModel> getStudentModels() {
+        return studentModels;
+    }
+
+    public ArtifactModel getArtifactModel() {
+        return artifactModel;
+    }
+
+    public String getVote() {
+        return vote;
+    }
+
+    public void setVote(String vote) {
+        this.vote = vote;
+    }
+
+    public Integer getTeamId() {
+        return teamId;
+    }
+
+    public String getNameTeam() {
+        return nameTeam;
+    }
 }
