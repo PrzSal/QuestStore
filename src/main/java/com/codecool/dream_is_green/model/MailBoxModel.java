fb65d5c60fc5daf6b12e873dd67ac8e33150c873
Przemo8922 <przemyslawsalak@gmail.com>
2017-11-22 01:27:04 +0100

    small change in MailBoxModel

--- src/main/java/com/codecool/dream_is_green/model/MailBoxModel.java
+++ src/main/java/com/codecool/dream_is_green/model/MailBoxModel.java
@@ -5,13 +5,15 @@ public class MailBoxModel extends User {
     private String content;
     private String header;
     private Integer idRecipient;
+    private Integer read;
 
     public MailBoxModel(int userID, String name, String surname, String email,
-                        String login, String password, String header, String content, Integer idRecipient) {
+                        String login, String password, String header, String content, Integer idRecipient, Integer read) {
         super(userID, name, surname, email, login, password);
         this.content = content;
         this.header = header;
         this.idRecipient = idRecipient;
+        this.read = read;
     }
 
     public String getContent() {
@@ -24,5 +26,9 @@ public class MailBoxModel extends User {
 
     public Integer getIdRecipient() {
         return idRecipient;
+    }
+
+    public Integer getRead() {
+        return read;
     }
 }
