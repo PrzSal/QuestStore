a00fe58f5a27c275808b4ae3ea93d90d0fbf4c33
PawelSyktus <pawel.syktus@gmail.com>
2017-11-28 19:55:07 +0100

    Add sendQuestResponse

--- src/main/java/com/codecool/dream_is_green/controller/ResponseController.java
+++ src/main/java/com/codecool/dream_is_green/controller/ResponseController.java
@@ -1,6 +1,7 @@
 package com.codecool.dream_is_green.controller;
 
 import com.codecool.dream_is_green.dao.TeamDao;
+import com.codecool.dream_is_green.model.QuestModel;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
@@ -18,6 +19,27 @@ public class ResponseController<T> {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/main.twig");
         JtwigModel model = JtwigModel.newModel();
 
+        model.with(objectModels, objectsList);
+        model.with("title", title);
+        model.with("menu", "classpath:/templates/" + menuPath);
+        model.with("main", "classpath:/templates/" + pagePath);
+        model.with("counterMail", counterMail);
+        String response = template.render(model);
+
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+
+    public void sendQuestResponse(HttpExchange httpExchange, Integer counterMail, LinkedList<T> objectsList,
+                                  LinkedList<QuestModel> foundObject, String objectModels, String title,
+                                  String menuPath, String pagePath) throws IOException {
+
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/main.twig");
+        JtwigModel model = JtwigModel.newModel();
+
+        model.with("studentWithQuest", foundObject);
         model.with(objectModels, objectsList);
         model.with("title", title);
         model.with("menu", "classpath:/templates/" + menuPath);
